9b093e58e95da0a7ad42e3a142eb4d14be626c27
m-donohoe <martiedonohoe@gmail.com>
2016-05-27 20:44:19 +0100

    Create reverse.js

---
+++ scripts/reverse.js
@@ -0,0 +1,9 @@
+<!-- Begin
+function reverse(form) {
+	text = "";
+	str = form.revtext.value;
+	for (i = 0; i <= str.length; i++)
+	text = str.substring(i, i+1) + text;
+	form.revtext.value = text;
+	}
+//  End -->
